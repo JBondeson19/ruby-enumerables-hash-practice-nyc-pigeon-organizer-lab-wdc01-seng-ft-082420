156b93760cca6e11ba736fa84713369256bb3f3f
James P Bondeson <jpbond58@gmail.com>
2020-08-18 04:13:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
             if !final_array[name][key]
               final_array[name][key] = []
           end
-          inner_key << final_array[name][key]
+          final_array[name][key].push(inner_key)
         end
       end
     end
