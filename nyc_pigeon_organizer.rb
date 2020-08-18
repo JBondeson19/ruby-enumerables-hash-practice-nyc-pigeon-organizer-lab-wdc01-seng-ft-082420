b914c8113c60ee9b703d2314ad9fba236133ed9a
James P Bondeson <jpbond58@gmail.com>
2020-08-18 03:58:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,12 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   data.with_each_object({}) do |(key,value), final_array|
 
-    value.each do|inner_key, names|  
+    value.each do|inner_key, names|
+      binding.pry
+      
+    end
     final_array
+    
   end
 
 end
